e9a57687d441272320d4951a95e0fe6bd00afefd
Octávio Ruiz Thomas <octaviorthomas@gmail.com>
2023-11-13 21:39:58 -0300

    Atualizações e melhorias aplicadas

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,64 @@
+
+using Biblioteca.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Biblioteca.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        public IActionResult Sair()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index", "Home");
+        }
+
+        public IActionResult Administrador()
+        {
+            return View();
+        }
+
+        public IActionResult ListaDeUsuarios()
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+
+            return View(new UsuarioService().Listar());
+        }
+
+        public IActionResult RegistrarUsuarios()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult RegistrarUsuarios(Usuario novoUser)
+        {
+            novoUser.Senha = Criptografo.TextoCriptografado(novoUser.Senha);
+
+            new UsuarioService().incluirUsuario(novoUser);
+
+            return RedirectToAction("ListaDeUsuarios");
+        }
+
+        public IActionResult EditarUsuario(int Id)
+        {
+            Autenticacao.CheckLogin(this);
+            Autenticacao.verificaSeUsuarioEAdmin(this);
+
+
+            return View(new UsuarioService().Listar(Id));
+        }
+
+        [HttpPost]
+        public IActionResult EditarUsuario(Usuario userEditado)
+        {
+            new UsuarioService().editarUsuario(userEditado);
+            return RedirectToAction("ListaDeUsuarios");
+        }
+
+         public IActionResult ExcluirUsuario(int Id)
+        {
+            new UsuarioService().excluirUsuario(Id);
+            return RedirectToAction("ListaDeUsuarios");
+        }
+    }
+}
